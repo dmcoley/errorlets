41b117dbc3a2d593b84ffc5ac4d4971e8d210362
Austin Bisharat <austinbisharat@gmail.com>
2016-03-08 16:37:27 -0800

    some crappy code

--- objects.js
+++ objects.js
@@ -1,1 +1,36 @@
 /* Object files go here */
+
+function StateMachine(f) {
+    this.errorHandler = null;
+    this.successHandler = f;
+}
+
+StateMachine.prototype.StateMachine = function () {
+    return this;
+}
+
+// Lift into state machine
+Function.prototype.StateMachine = function() {
+    var f = this; // the function
+
+    return new StateMachine(function(x, k) {
+	var res = f(x);
+	schedule(k, res);
+    });
+}
+
+function schedule(f, x) {
+    setTimeout(function() { f(x); }, 0);
+}
+
+StateMachine.prototype.next = function (g) {
+    var f = this;
+    g = g.StateMachine();
+
+    return new StateMachine(function (x, k) {
+	f.successHandler(x, function (y) {
+	    g.successHandler(y, k);
+	});
+    });
+}
+
