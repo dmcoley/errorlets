9ebfc55b21e74c1e0d72ca4cb2f7a3feae06060c
dmcoley <dmcoley@uw.edu>
2016-03-13 15:39:01 -0700

    hard map

---
+++ demos/hardutil.js
@@ -0,0 +1,22 @@
+function handleServerIsUp() {
+    var name = $("server").value;
+    $("results").innerHTML = "Server " + name + " is up :)";
+}
+
+function handleServerIsDown() {
+    var name = $("server").value;
+    $("results").innerHTML = "Server " + name + " is down :(";
+}
+
+function $() {
+    if (arguments.length==1) {
+        return document.getElementById(arguments[0]);
+    }
+    var result=[], i=0, el;
+    while(el=document.getElementById(arguments[i++])) {
+        result.push(el);
+    }
+    return result;
+}
+
+
