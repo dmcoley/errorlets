a205b75f23c1316ca0de11d20587704d4af09020
dmcoley <dmcoley@uw.edu>
2016-03-13 13:48:45 -0700

    easy demo

--- util.js
+++ util.js
@@ -1,3 +1,16 @@
 function schedule(f, x, id, until) {
     setTimeout(function() { f(x, id, until); }, 0);
 }
+
+function $() {
+    if (arguments.length==1) {
+        return document.getElementById(arguments[0]);
+    }
+
+    var result=[], i=0, el;
+    while(el=document.getElementById(arguments[i++])) {
+        result.push(el);
+    }
+
+    return result;
+}
